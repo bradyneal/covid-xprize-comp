c655a75977853622d124f10ce28e032940b0c4f0
Diogo Pernes <diogo.pernes.cunha@gmail.com>
2020-12-03 13:56:04 +0000

    Enhancements on data loading and preprocessing: - fills missing values - loads and merges demographic df with number of cases df - adds extra columns with preprocessed data (7-day smoothing, prediction ratio, etc.)

--- ongoing/predictors/base.py
+++ ongoing/predictors/base.py
@@ -19,10 +19,35 @@ OUR_FOLDER = 'ongoing'
 DATA_FOLDER = 'data'
 OXFORD_FILEPATH = os.path.join(OUR_FOLDER, DATA_FOLDER, 'OxCGRT_latest.csv')
 OXFORD_URL = 'https://raw.githubusercontent.com/OxCGRT/covid-policy-tracker/master/data/OxCGRT_latest.csv'
+ADDITIONAL_CONTEXT_FILE = os.path.join(OUR_FOLDER, DATA_FOLDER, "Additional_Context_Data_Global.csv")
+ADDITIONAL_US_STATES_CONTEXT = os.path.join(OUR_FOLDER, DATA_FOLDER, "US_states_populations.csv")
+ADDITIONAL_UK_CONTEXT = os.path.join(OUR_FOLDER, DATA_FOLDER, "uk_populations.csv")
+US_PREFIX = "United States / "
 
+NPI_COLUMNS = ['C1_School closing',
+               'C2_Workplace closing',
+               'C3_Cancel public events',
+               'C4_Restrictions on gatherings',
+               'C5_Close public transport',
+               'C6_Stay at home requirements',
+               'C7_Restrictions on internal movement',
+               'C8_International travel controls',
+               'H1_Public information campaigns',
+               'H2_Testing policy',
+               'H3_Contact tracing',
+               'H6_Facial Coverings']
+
+CONTEXT_COLUMNS = ['CountryName',
+                   'RegionName',
+                   'GeoID',
+                   'Date',
+                   'ConfirmedCases',
+                   'ConfirmedDeaths',
+                   'Population']
 
 def load_train_test(n_test_months=1, end_month=11, n_test_days=None,
-                    start_month=None, update_data=False):
+                    start_month=None, window_size=7, dropifnocases=True,
+                    dropifnodeaths=False, update_data=False):
     """
     Loads the data and splits it into train and test sets
 
@@ -54,6 +79,41 @@ def load_train_test(n_test_months=1, end_month=11, n_test_days=None,
                          error_bad_lines=False)
         print('Using existing data up to date {}'.format(str(df.Date[len(df) - 1]).split()[0]))
 
+    # Add unique identifier for each location (region + country)
+    df["GeoID"] = np.where(df["RegionName"].isnull(),
+                           df["CountryName"],
+                           df["CountryName"] + ' / ' + df["RegionName"])
+
+    # Load dataframe with demographics about each country
+    context_df = load_additional_context_df()
+
+    # Merge the two dataframes
+    df = df.merge(context_df, on=['GeoID'], how='left', suffixes=('', '_y'))
+
+    # Fill in missing values
+    fill_missing_values(df, dropifnocases=dropifnocases, dropifnodeaths=dropifnodeaths)
+
+    # Compute number of new cases and deaths each day
+    df['NewCases'] = df.groupby('GeoID').ConfirmedCases.diff().fillna(0)
+    df['NewDeaths'] = df.groupby('GeoID').ConfirmedDeaths.diff().fillna(0)
+    # Replace negative values (which do not make sense for these columns) with 0
+    df['NewCases'] = df['NewCases'].clip(lower=0)
+    df['NewDeaths'] = df['NewDeaths'].clip(lower=0)
+    # Compute smoothed versions of new cases and deaths each day
+    df['SmoothNewCases'] = df.groupby('GeoID')['NewCases'].rolling(
+        window_size, center=False).mean().fillna(0).reset_index(0, drop=True)
+    df['SmoothNewDeaths'] = df.groupby('GeoID')['NewDeaths'].rolling(
+        window_size, center=False).mean().fillna(0).reset_index(0, drop=True)
+    # Compute percent change in new cases and deaths each day
+    df['CaseRatio'] = df.groupby('GeoID').SmoothNewCases.pct_change(
+    ).fillna(0).replace(np.inf, 0) + 1
+    df['DeathRatio'] = df.groupby('GeoID').SmoothNewDeaths.pct_change(
+    ).fillna(0).replace(np.inf, 0) + 1
+    # Add column for proportion of population infected
+    df['ProportionInfected'] = df['ConfirmedCases'] / df['Population']
+    # Create column of value to predict
+    df['PredictionRatio'] = df['CaseRatio'] / (1 - df['ProportionInfected'])
+
     assert 1 <= end_month <= 11
     end_test = datetime(2020, end_month + 1, 1)
     if start_month is not None:
@@ -75,6 +135,80 @@ def load_train_test(n_test_months=1, end_month=11, n_test_days=None,
         train_df = df[df['Date'] < start_test]
 
     return train_df, test_df
+
+
+def load_additional_context_df():
+    # File containing the population for each country
+    # Note: this file contains only countries population, not regions
+    additional_context_df = pd.read_csv(ADDITIONAL_CONTEXT_FILE,
+                                        usecols=['CountryName', 'Population'])
+    additional_context_df['GeoID'] = additional_context_df['CountryName']
+
+    # US states population
+    additional_us_states_df = pd.read_csv(ADDITIONAL_US_STATES_CONTEXT,
+                                          usecols=['NAME', 'POPESTIMATE2019'])
+    # Rename the columns to match measures_df ones
+    additional_us_states_df.rename(columns={'POPESTIMATE2019': 'Population'}, inplace=True)
+    # Prefix with country name to match measures_df
+    additional_us_states_df['GeoID'] = US_PREFIX + additional_us_states_df['NAME']
+
+    # Append the new data to additional_df
+    additional_context_df = additional_context_df.append(additional_us_states_df)
+
+    # UK population
+    additional_uk_df = pd.read_csv(ADDITIONAL_UK_CONTEXT)
+    # Append the new data to additional_df
+    additional_context_df = additional_context_df.append(additional_uk_df)
+
+    return additional_context_df
+
+
+def fill_missing_values(df, dropifnocases=True, dropifnodeaths=False):
+    df.update(df.groupby('GeoID').ConfirmedCases.apply(
+        lambda group: group.interpolate(limit_area='inside')))
+    if dropifnocases:
+        # Drop country / regions for which no number of cases is available
+        df.dropna(subset=['ConfirmedCases'], inplace=True)
+        df.update(df.groupby('GeoID').ConfirmedDeaths.apply(
+            lambda group: group.interpolate(limit_area='inside')))
+    if dropifnodeaths:
+        # Drop country / regions for which no number of deaths is available
+        df.dropna(subset=['ConfirmedDeaths'], inplace=True)
+        for npi_column in NPI_COLUMNS:
+            df.update(df.groupby('GeoID')[npi_column].ffill().fillna(0))
+
+
+def convert_ratio_to_new_cases(ratio,
+                               window_size,
+                               prev_new_cases_list,
+                               prev_pct_infected):
+        return (ratio * (1 - prev_pct_infected) - 1) * \
+               (window_size * np.mean(prev_new_cases_list[-window_size:])) \
+               + prev_new_cases_list[-window_size]
+
+
+def convert_ratios_to_total_cases(self,
+                                  ratios,
+                                  window_size,
+                                  prev_new_cases,
+                                  initial_total_cases,
+                                  pop_size):
+    new_new_cases = []
+    prev_new_cases_list = list(prev_new_cases)
+    curr_total_cases = initial_total_cases
+    for ratio in ratios:
+        new_cases = self._convert_ratio_to_new_cases(ratio,
+                                                     window_size,
+                                                     prev_new_cases_list,
+                                                     curr_total_cases / pop_size)
+        # new_cases can't be negative!
+        new_cases = max(0, new_cases)
+        # Which means total cases can't go down
+        curr_total_cases += new_cases
+        # Update prev_new_cases_list for next iteration of the loop
+        prev_new_cases_list.append(new_cases)
+        new_new_cases.append(new_cases)
+    return new_new_cases
 
 
 class BasePredictorMeta(ABCMeta):
@@ -135,11 +269,11 @@ class BasePredictor(object, metaclass=BasePredictorMeta):
 
     def choose_train_test_split(self, n_test_months=1, end_month=11,
                                 n_test_days=None, start_month=None,
-                                update_data=False):
+                                window_size=7, update_data=False):
         self.train_df, self.test_df = \
             load_train_test(n_test_months=n_test_months, end_month=end_month,
                             n_test_days=n_test_days, start_month=start_month,
-                            update_data=update_data)
+                            window_size=window_size, update_data=update_data)
 
     def train_df(self):
         return self.train_df
