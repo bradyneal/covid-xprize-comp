15aac2ee0ad4a5084da5d7c44ed420a88bb10503
Diogo Pernes <diogo.pernes.cunha@gmail.com>
2021-01-16 17:14:20 +0000

    greedy_prescriptor reimplemented as a child of BasePrescriptor

---
+++ ongoing/prescriptors/blind_greedy/prescribe.py
@@ -0,0 +1,118 @@
+# Copyright 2020 (c) Cognizant Digital Business, Evolutionary AI. All rights reserved. Issued under the Apache 2.0 License.
+
+"""
+This is the prescribe.py script for a simple example prescriptor that
+generates IP schedules that trade off between IP cost and cases.
+
+The prescriptor is "blind" in that it does not consider any historical
+data when making its prescriptions.
+
+The prescriptor is "greedy" in that it starts with all IPs turned off,
+and then iteratively turns on the unused IP that has the least cost.
+
+Since each subsequent prescription is stricter, the resulting set
+of prescriptions should produce a Pareto front that highlights the
+trade-off space between total IP cost and cases.
+
+Note this file has significant overlap with ../random/prescribe.py.
+"""
+
+import os
+import argparse
+import numpy as np
+import pandas as pd
+
+from ongoing.prescriptors.blind_greedy.blind_greedy_prescriptor import BlindGreedy
+
+NUM_PRESCRIPTIONS = 10
+
+IP_MAX_VALUES = {
+    'C1_School closing': 3,
+    'C2_Workplace closing': 3,
+    'C3_Cancel public events': 2,
+    'C4_Restrictions on gatherings': 4,
+    'C5_Close public transport': 2,
+    'C6_Stay at home requirements': 3,
+    'C7_Restrictions on internal movement': 2,
+    'C8_International travel controls': 4,
+    'H1_Public information campaigns': 2,
+    'H2_Testing policy': 3,
+    'H3_Contact tracing': 2,
+    'H6_Facial Coverings': 4
+}
+
+IP_COLS = list(IP_MAX_VALUES.keys())
+
+
+def prescribe(start_date_str: str,
+              end_date_str: str,
+              path_to_hist_file: str,
+              path_to_cost_file: str,
+              output_file_path) -> None:
+
+    # Load historical IPs, just to extract the geos
+    # we need to prescribe for.
+    hist_df = pd.read_csv(path_to_hist_file,
+                          parse_dates=['Date'],
+                          encoding="ISO-8859-1",
+                          keep_default_na=False,
+                          error_bad_lines=True)
+    hist_df["RegionName"] = hist_df["RegionName"].fillna('')
+    hist_df["GeoID"] = np.where(hist_df["RegionName"] == '',
+                                hist_df["CountryName"],
+                                hist_df["CountryName"] + ' / ' + hist_df["RegionName"])
+
+    # Load the IP weights, so that we can use them
+    # greedily for each geo.
+    weights_df = pd.read_csv(path_to_cost_file, keep_default_na=False)
+    weights_df["RegionName"] = weights_df["RegionName"].fillna('')
+    weights_df["GeoID"] = np.where(weights_df["RegionName"] == '',
+                                   weights_df["CountryName"],
+                                   weights_df["CountryName"] + ' / ' + weights_df["RegionName"])
+
+    # instantiate the prescriptor and generate the prescriptions
+    prescriptor = BlindGreedy()
+    prescription_df = prescriptor.prescribe(start_date_str, end_date_str, hist_df, weights_df)
+
+    # Create the directory for writing the output file, if necessary.
+    os.makedirs(os.path.dirname(output_file_path), exist_ok=True)
+
+    # Save output csv file.
+    prescription_df.to_csv(output_file_path, index=False)
+
+    return
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--start_date",
+                        dest="start_date",
+                        type=str,
+                        required=True,
+                        help="Start date from which to prescribe, included, as YYYY-MM-DD."
+                             "For example 2020-08-01")
+    parser.add_argument("-e", "--end_date",
+                        dest="end_date",
+                        type=str,
+                        required=True,
+                        help="End date for the last prescription, included, as YYYY-MM-DD."
+                             "For example 2020-08-31")
+    parser.add_argument("-ip", "--interventions_past",
+                        dest="prev_file",
+                        type=str,
+                        required=True,
+                        help="The path to a .csv file of previous intervention plans")
+    parser.add_argument("-c", "--intervention_costs",
+                        dest="cost_file",
+                        type=str,
+                        required=True,
+                        help="Path to a .csv file containing the cost of each IP for each geo")
+    parser.add_argument("-o", "--output_file",
+                        dest="output_file",
+                        type=str,
+                        required=True,
+                        help="The path to an intervention plan .csv file")
+    args = parser.parse_args()
+    print(f"Generating prescriptions from {args.start_date} to {args.end_date}...")
+    prescribe(args.start_date, args.end_date, args.prev_file, args.cost_file, args.output_file)
+    print("Done!")
