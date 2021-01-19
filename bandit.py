e7011417dac2f9f7dd6e279061b9b8e9a4db9401
Baihan Lin <doerlbh@gmail.com>
2021-01-19 16:32:40 -0500

    [FEAT] finish bandit algorithm

--- bandit.py
+++ bandit.py
@@ -7,7 +7,10 @@ Bandit/RL Class
 usage:
 
 from bandit import CCTSB
-bandit = CCTSB(N=10, K=5, C=None, alpha_p=None, lambda_p=None)
+bandit = CCTSB(N=10, K=5, alpha_p=0.5, lambda_p=0.5)
+bandit.observe(context)
+actions = bandit.act()
+bandit.update(reward,cost)
 
 """
 
@@ -17,7 +20,6 @@ class Agent(object):
     def __init__(self):
         self.N = None
         self.K = None
-        self.C = None
 
     def observe(self):
         raise NotImplementedError
@@ -30,46 +32,40 @@ class Agent(object):
             
 
 class CCTSB(Agent):
-    def __init__(self, N=None, K=None, C=None, alpha_p=None, lambda_p=None):
-        self.N = N
-        self.K = K
-        self.C = C
+    def __init__(self, N=None, K=None, alpha_p=None, lambda_p=None):
+        self.N = N # number of possible values in each action
+        self.K = K # number of possible intervention actions
+        
         self.alpha = alpha_p
         self.lambda = lambda_p
         
-        self.A_k = self.K * [np.eye(N+1)]
-        self.g_k = self.K * [np.zeros((N+1,N+1))]
-        self.mu_k = self.K * [np.zeros((N+1,N+1))]
-        self.B_i = self.N * [np.eye(N+1)]
-        self.z_i = self.N * [np.zeros((N+1,N+1))]
-        self.theta_i = self.N * [np.zeros((N+1,N+1))]
+        self.B_i = self.K * [ self.N * [np.eye(N+1)] ]
+        self.z_i = self.K * [ self.N * [np.zeros((N+1,N+1))] ]
+        self.theta_i = self.K * [ self.N * [np.zeros((N+1,N+1))] ]
         
-        self.c = None
-        self.k = None
+        self.c_t = None
+        self.i_t = None
         
     def observe(self, c):
-        self.c = c
+        self.c_t = c
 
     def act(self):
-        sample_mu = np.zeros((self.K,self.N))
         sample_theta = np.zeros((self.K,self.N))
-        sample_to_sort = []
+        i_t = {}
         for k in range(self.K):
             for i in range(self.N):
-                sample_mu[k,i] = np.random.multivariate_normal(self.mu_k[k], self.alpha**2 * np.inv(self.A_k[k]))
-                sample_theta[k,i] = np.random.multivariate_normal(self.theta_i[i], self.alpha**2 * np.inv(self.B_i[i]))
-            sample_to_sort.append( self.c.T.dot(sample_mu[k]) / self.c.T.dot(sample_theta[k]) )
-        # TODO clarify step 9 and 10   
+                sample_theta[k,i] = np.random.multivariate_normal(self.theta_i[k][i], self.alpha**2 * np.inv(self.B_i[k][i]))
+            i_t[k] = np.argmax( self.c_t.T.dot(sample_theta[k]) ) 
+        self.i_t = i_t
+        return i_t
     
     def update(self, r=None, s=None):
-        # TODO clarify step 14
-        self.A_k[self.k] += self.c.dot(self.c.T)
-        self.g_k[self.k] += self.c * r
-        self.mu_k[self.k] = np.inv(self.A_k[self.k]).dot(self.g_k[self.k])
-        for i in range(self.N):
-            self.B_i[i] = self.lambda * self.B_i[i] + self.c.dot(self.c.T)
-            self.z_i[i] += self.c * s
-            self.theta_i[i] = np.inv(self.B_i[i]).dot(self.z_i[i])
+        r_star = r/s
+        for k in range(self.K):
+            i = self.i_t[k]
+            self.B_i[k][i] = self.lambda * self.B_i[k][i] + self.c_t.dot(self.c_t.T)
+            self.z_i[k][i] += self.c * r_star
+            self.theta_i[k][i] = np.inv(self.B_i[k][i]).dot(self.z_i[k][i])
             
         
             
