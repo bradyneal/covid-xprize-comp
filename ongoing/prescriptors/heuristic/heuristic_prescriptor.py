51567b49ae52b5dbf48b8cade655b8bdee37e1a0
Thin Nguyen <nkthin@gmail.com>
2021-01-29 12:14:40 +1100

    Add files via upload

--- ongoing/prescriptors/heuristic/heuristic_prescriptor.py
+++ ongoing/prescriptors/heuristic/heuristic_prescriptor.py
@@ -41,32 +41,50 @@ class Heuristic(BasePrescriptor):
 
                 # Sort IPs for this geo by weight
                 geo_costs = cost_df[cost_df['GeoID'] == geoid][NPI_COLUMNS]
+                ip_names = list(geo_costs.columns)
                 ip_weights = geo_costs.values[0]
-#                 normalized
                 ip_weights = [e/sum(ip_weights) for e in ip_weights]
 
+#                 sorted_ips = [ip for _, ip in sorted(zip(ip_weights, ip_names))]
+
+                # Initialize the IPs to all turned off
+#                 curr_ips = {ip: 0 for ip in NPI_MAX_VALUES}
+
+
+
                 for prescription_idx in range(NUM_PRESCRIPTIONS):
+                    P =  (prescription_idx + 1)*PCT
+                    P = np.percentile(np.array(ip_weights), P)
+
+                    # Turn on the next IP
+#                     next_ip = sorted_ips[prescription_idx]
+#                     curr_ips[next_ip] = NPI_MAX_VALUES[next_ip]
+
+                    # Use curr_ips for all dates for this prescription
                     for date in pd.date_range(start_date, end_date):
                         prescription_dict['PrescriptionIndex'].append(prescription_idx)
                         prescription_dict['CountryName'].append(country_name)
                         prescription_dict['RegionName'].append(region_name)
                         prescription_dict['Date'].append(date.strftime("%Y-%m-%d"))
+#                         for ip in NPI_COLUMNS:
+#                             prescription_dict[ip].append(curr_ips[ip])
                         for ii in range(len(NPI_COLUMNS)):
-                            v = 0    
-                            if (ip_weights[ii] < (0.05 + 0.01*prescription_idx)):
+                            v = 0  
+                            if (ip_weights[ii] <= P):
                                 v = NPI_MAX_VALUES[NPI_COLUMNS[ii]]
-                            prescription_dict[NPI_COLUMNS[ii]].append(v)
+                            prescription_dict[NPI_COLUMNS[ii]].append(v)                            
 
         # Create dataframe from dictionary.
         prescription_df = pd.DataFrame(prescription_dict)
 
         return prescription_df
-    
-if __name__ == '__main__':
+
+# if __name__ == '__main__':
+for PCT in [5]:#,6,7,8,9,10,9.25,9.5,9.75]:
+    st = str(PCT).replace('.','_')
+    print('\nRUNNING ',st)
     prescriptor = Heuristic()
     # output_dir = '/home/thinng/code/2020/covid-xprize-comp/prescriptions/'
     output_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir, os.pardir, os.pardir, 'prescriptions')
-    ofile_path = os.path.abspath(os.path.join(output_dir, 'Heuristic.csv'))
-    print(ofile_path)
-    print()
+    ofile_path = os.path.abspath(os.path.join(output_dir, 'Heuristic_pct_' + st + '.csv'))
     prescriptor.evaluate(output_file_path=ofile_path)
